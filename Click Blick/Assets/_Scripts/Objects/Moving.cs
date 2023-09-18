7c56f502964e39cecf966cbd51982b3120394902
KatyaZav <party2015ru@mail.ru>
2023-09-18 22:29:22 +0500

    object moving added

---
+++ Click Blick/Assets/_Scripts/Objects/Moving.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving : MonoBehaviour
+{
+    private GameObject left;
+    private GameObject right;
+    public float speed;
+
+    private void Awake()
+    {
+        left = GameObject.FindGameObjectWithTag("Left");
+        right = GameObject.FindGameObjectWithTag("Right");
+        
+        StartCoroutine(moveChous());
+    }
+    
+
+    private IEnumerator moveChous()
+    {
+        while (true)
+        {
+            float x = Random.Range(left.transform.position.x, right.transform.position.x);
+            float y = Random.Range(left.transform.position.y, right.transform.position.y);
+            Vector2 B = new Vector2(x, y);
+
+            while (distance(transform, B) > 1)
+            {
+                transform.position = Vector2
+                    .Lerp(transform.position, B, speed / (distance(transform, B) * 300));
+                yield return new WaitForFixedUpdate();
+            }
+        }
+    }
+
+    private float distance(Transform A, Vector2 B)
+    {
+        float a = Mathf.Abs(B.x - A.position.x);
+        float b = Mathf.Abs(B.y - A.position.y);
+
+        return Mathf.Sqrt(a * a + b * b);
+    }
+
+    private void OnDestroy()
+    {
+        StopAllCoroutines();
+    }
+}
