f92e1b6beb77561a77355b03db81452fc7cb75af
KatyaZav <party2015ru@mail.ru>
2023-12-13 23:05:19 +0500

    fux box particle effect bug

--- Click Blick/Assets/_Scripts/Objects/Box.cs
+++ Click Blick/Assets/_Scripts/Objects/Box.cs
@@ -17,7 +17,7 @@ public class Box : MonoBehaviour
 
     private void Start()
     {
-        BallLogic.StartedAgain += OnDestroy;
+        BallLogic.StartedAgain += OnDestroyd;
 
         if (maxScale > 1)
             transform.localScale =  new Vector3(Random.Range(1, maxScale), 1, 1);
@@ -25,12 +25,12 @@ public class Box : MonoBehaviour
         lifeTime = Random.Range(minLifeTime, maxLifeTime);
         transform.Rotate(new Vector3(0, 0, Random.Range(0, 180))); 
 
-        Invoke("OnDestroy", lifeTime);
+        Invoke("OnDestroyd", lifeTime);
         sprite = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
     }
 
-    private void OnDestroy()
+    private void OnDestroyd()
     {
         try{
             if (effect != null)
@@ -45,7 +45,7 @@ public class Box : MonoBehaviour
             sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, 0.5f);
             anim.SetTrigger("destroy");
 
-            BallLogic.StartedAgain -= OnDestroy;
+            BallLogic.StartedAgain -= OnDestroyd;
             Destroy(gameObject, 0.2f);
         }
         catch (System.Exception e)
